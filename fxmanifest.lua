f8a0fb9f0889b0d9089fbcaf05b1b1f678b2e5b4
Howsn <43304111+Howsn@users.noreply.github.com>
2024-12-18 19:49:03 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Howsn'
+description 'Stormram script for ox_doorlock!'
+version '1.0.0'
+
+shared_scripts {
+  '@ox_lib/init.lua'
+}
+
+client_scripts {
+  'client/client.lua',
+}
+
+dependencies {
+  'ox_lib',
+  'ox_target',
+  'ox_inventory',
+  'ox_doorlock',
+}
+
+lua54 'yes'
