38a2f2172f738481feb1826b714d72b9489f3462
Howsn <43304111+Howsn@users.noreply.github.com>
2024-12-26 18:52:38 +0100

    Renamed event

--- server/server.lua
+++ server/server.lua
@@ -1,4 +1,4 @@
-RegisterNetEvent('qbx_stormram:server:setState')
-AddEventHandler('qbx_stormram:server:setState', function(doorId, state)
+RegisterNetEvent('howsn_stormram:server:setState')
+AddEventHandler('howsn_stormram:server:setState', function(doorId, state)
     TriggerEvent('ox_doorlock:setState', doorId, state)
 end)
